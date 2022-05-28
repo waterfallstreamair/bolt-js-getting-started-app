0ddaed671a67a1a4b60bcc11d720d6f33674f4f5
waterfallstreamair <waterfallstreamair@gmail.com>
2022-05-28 11:10:42 +0000

    User story 1: get slack channel history, add channel to command, improve output of responce time, add read channels permission.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { App } = require('@slack/bolt');
-//import moment from 'moment'
+const moment = require('moment')
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
@@ -37,48 +37,72 @@ const app = new App({
 
 let db = []
 //let responses = []
-const saveToDatabase = params => {
+/*const saveToDatabase = params => {
   db.push(params)
-}
+}*/
 
-const getResponses = ({ userId, seconds, startDate, endDate }) => {
+const getResponses = async ({ userId, seconds, startDate, endDate, channel, client }) => {
   let responses = []
   let prevTs = 0
   let step = 0
-  db.forEach(e => {
+  const result = await client.conversations.list()
+  //console.log({ rch: result.channels })
+  const channelId = (result.channels.find(e => e.name == channel) || {}).id
+  //console.log('Found channel id:')
+  //console.log({ channelId })
+  db = await client.conversations.history({
+      channel: channelId,
+      //latest: startDate,
+      //oldest: endDate,
+      //inclusive: true
+  })
+  //console.log({ db })
+  //console.log({ messages: db.messages })
+  db.messages.forEach(e => {
     if (e.text.includes(userId) && step == 0) {
-      //say(e.text)
-      if (!startDate && !endDate) {
-        step = 1
-        prevTs = e.ts
-      }
-      if (startDate && endDate && 
-        Date.parse(startDate) < Date.parse(e.ts) &&
-        Date.parse(endDate) > Date.parse(e.ts)
-      ) {
-        step = 1
-        prevTs = e.ts
-      }
+      console.log({ step, userId, e  })
+      step = 1
+      prevTs = e.ts
     } else if (e.user == userId && step == 1) {
-      console.log({ ts: e.ts, prevTs })
-      const rt = +e.ts - prevTs
+      const rt = +prevTs - e.ts
+      console.log({ step, userId, e, rt })
+      /*
       if ((seconds && rt < seconds) || !seconds) {
         responses.push({ user: userId, rt, text: e.text, ts: e.ts })
         step = 0
-        console.log({ responses })
       }
+      */
+      responses.push({ user: userId, rt, text: e.text, ts: e.ts })
+      step = 0
     }
   })
+  console.log({ responses })
   return responses
 }
 
+const humanizeDuration = d => {
+  const s = d.get('seconds') ? `${d.get('seconds')}s ` : ''
+  const m = d.get('minutes') ? `${d.get('minutes')}m ` : ''
+  const h = d.get('hours') ? `${d.get('hours')}h ` : ''
+  const days = d.get('days') ? `${d.get('days')} days ` : ''
+  const months = d.get('months') ? `${d.get('months')} months ` : ''
+  return `${months}${days}${h}${m}${s}`
+}
+
 const getStatistics = ({ responses }) => {
-  const nums = responses.map(e => e.rt)
-  if (nums.length > 0) {
+  if (responses && responses.length) {
+    const nums = responses.map(e => e.rt)
+    const min = Math.min(...nums)
+    const max = Math.max(...nums)
+    const avg = (nums.reduce((a, b) => a + b) / nums.length)
+    /*const threshold = { d: 7, h: 24, m: 60, s: 60, ss: 0 }
+    console.log({ 
+      min: humanizeDuration(moment.duration(min, 'seconds'))
+    })*/
     return {
-      min: Math.floor(Math.min(...nums)),
-      max: Math.floor(Math.max(...nums)),
-      avg: Math.floor(nums.reduce((a, b) => a + b) / nums.length)
+      min: humanizeDuration(moment.duration(min, 'seconds')),
+      max: humanizeDuration(moment.duration(max, 'seconds')),
+      avg: humanizeDuration(moment.duration(avg, 'seconds')),
     }
   } 
   return {
@@ -88,30 +112,31 @@ const getStatistics = ({ responses }) => {
 
 const userToId = async ({ userName, client }) => {
   const result = await client.users.list();
-  //console.log({ result })
-  //console.log({ members: result.members })
-  return (result.members.find(item => item.name == userName) || {}).id
+  /*console.log({ result })
+  console.log({ members: result.members })
+  //console.log(result.members.map(e => e.profile))*/
+  return (result.members.find(item => 
+    [item.name, item.profile.display_name].includes(userName)) || {}).id
 }
 
 app.message('', async ({ message, say, client, ...r }) => {
-  
   //console.log({ r })
-  console.log({ message, /* say */ })
-  
+  //console.log({ message, /* say */ })
   const thread_ts = message.thread_ts || message.ts;
+  
   /*await say({
     text: `Hello <@${message.user}>!`,
     thread_ts
   });*/
   
-  const { user, ts, channel, client_msg_id, text } = message
+  /*const { user, ts, channel, client_msg_id, text } = message
   saveToDatabase({
     client_msg_id, user, ts, thread_ts, channel, text
   })
-  console.log({ db })
+  console.log({ db })*/
   
   if (message.text.includes('show-user-info')) {
-    const [text, userName, channel] = message.text.split(' ')
+    const [text, userName, channel, startDate, endDate] = message.text.split(' ')
     const userId = await userToId({ userName, client })
     if (!userId) {
       say('User not found')
@@ -120,7 +145,7 @@ app.message('', async ({ message, say, client, ...r }) => {
         text: `Analyzing...`,
         thread_ts
       })
-      const responses = getResponses({ userId })
+      const responses = await getResponses({ userId, channel, startDate, endDate, client })
       const { min, max, avg } = getStatistics({ responses })
       await say({
         text: `
@@ -139,6 +164,7 @@ app.message('', async ({ message, say, client, ...r }) => {
     }
   }
   
+  /*
   if (message.text.includes('show-users-log')) {
     const [text, seconds, startDate, endDate] =
       message.text.split(' ')
@@ -177,11 +203,11 @@ app.message('', async ({ message, say, client, ...r }) => {
       }
     })
   }
+  */
+  
 });
 
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3000);
-
   console.log('⚡️ Bolt app is running!');
 })();
