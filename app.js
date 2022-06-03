02b05689762a000f62a4425497fc6f2cd53f4890
waterfallstreamair <waterfallstreamair@gmail.com>
2022-06-03 12:40:18 +0000

    Add 2 slash commands - for user story 1 and user story 2.

--- app.js
+++ app.js
@@ -43,19 +43,19 @@ const channelToId = async ({ channel, client }) => {
 }
 
 const getResponses = async ({ 
-  userId, seconds, start, end, channel, client, say, event }) => {
+  userId, seconds, start, end, channelId, client, say, event }) => {
     console.log({ start, end })
   let responses = []
   let prevTs = 0
  
   const result = await client.conversations.list()
-  let channelId = (result.channels.find(e => e.name == channel) || {}).id
+  //let channelId = (result.channels.find(e => e.name == channel) || {}).id
   
-  if (!channelId) {
+  /*if (!channelId) {
     say('Channel not found. Using current channel')
     channelId = event.channel
     //return []
-  }
+  }*/
   
   db = await client.conversations.history({
     channel: channelId,
@@ -87,11 +87,11 @@ const getResponses = async ({
     replies.messages.forEach(r => {
       if (r.text.includes(`@${userId}`)) {
         prevTs = r.ts
-        console.log({ mentioned: r })
+        //console.log({ mentioned: r })
       } else if (r.user == userId && prevTs) {
         const rt = +r.ts - prevTs
         prevTs = null
-        console.log({ responced: r })
+        //console.log({ responced: r })
        
         responses.push({ user: userId, rt, text: r.text, ts: r.ts })
         //console.log({ responses })
@@ -100,7 +100,7 @@ const getResponses = async ({
     
   }))
   responses.sort((a, b) => a.rt - b.rt)
-  
+  console.log({ responses })
   return responses
 }
 
@@ -161,39 +161,9 @@ app.message('', async ({ message, say, client, event, ...r }) => {
   
   if (message.text.includes('show-user-info')) {
     //console.log({ message, client, r})
-    const [text, userName, channel, startDate, endDate] = message.text.split(' ')
-    let userId = await userToId({ userName, client }) 
-    if (!userId) {
-      await say({ 
-        text: 'User not found. Using current user ' + 
-          await idToUser({ userId: message.user, client }),
-          thread_ts 
-      })
-      userId = message.user
-    } 
-    await say({ text: `Analyzing...`, thread_ts })
-    const start = startDate ? moment(startDate) : moment().subtract(14, 'days')
-    const end = endDate ? moment(endDate) : moment()
-    const responses = await getResponses({ 
-      userId, channel, start, end, client, say 
-    })
-    const { min, max, avg } = getStatistics({ responses })
-    await say({
-      text: `
-        User id ${userId}
-        Statistics for period 
-          from ${start.format('DD-MM-yyyy')} 
-          to ${end.format('DD-MM-yyyy')}:
-        Min response = ${min || 'no data'} 
-        Max response = ${max || 'no data'} 
-        Avg response = ${avg || 'no data'} 
-      `,
-      thread_ts
-    });
-    await say({ text: `Done.`, thread_ts })
+
   }
-  
-  
+
   if (message.text.includes('show-users-log')) {
     const [text, channel, seconds, startDate, endDate] =
       message.text.split(' ')
@@ -225,7 +195,7 @@ app.message('', async ({ message, say, client, event, ...r }) => {
             await say({
               text: `
                 User: ${e.name || 'name not found'}: 
-                Date: ${moment(+responce.ts).format('DD-MM-yyyy hh:mm:ss') || 'time stamp not found'}
+                Date: ${moment.unix(+responce.ts).format('DD-MM-yyyy hh:mm:ss') || 'time stamp not found'}
                 Response: ${humanizeDuration(moment.duration(responce.rt, 'seconds')) || 'responce time not found'}
                 Text: ${responce.text || 'text not found'}
                 Link: ${`<${link.permalink}|...>`}
@@ -243,6 +213,134 @@ app.message('', async ({ message, say, client, event, ...r }) => {
   }
   
 });
+
+app.command('/show-user-info', 
+  async ({ command, ack, say, client, ...r }) => {
+    
+  await ack();
+  //console.log({ command, ack, respond, r })
+  console.log({ command })
+  const message = await client.chat.postMessage({ 
+    text: 'Show user info: ',
+    channel: command.channel_id
+  })
+  const thread_ts = message.ts
+  
+  const [user, channel, startDate, endDate] = command.text.split(' ')
+  
+    let userId = (user.split('|')[0]).replace('<@', '')
+    let userName = (user.split('|')[1]).replace('>', '')
+    console.log({ user, userId })
+    if (!userId) {
+      await say({ 
+        text: 'User not found. Using current user ' + 
+          command.user,
+          thread_ts 
+      })
+      userId = command.user_id
+    } 
+    
+    let channelId = (channel.split('|')[0]).replace('<#', '')
+    console.log({ channel, channelId })
+    if (!channelId) {
+      await say({ 
+        text: 'Channel not found. Using current channel ' + 
+          command.channel,
+          thread_ts 
+      })
+      channelId = command.channel_id
+    } 
+    
+    await say({ text: `Analyzing...`, thread_ts })
+    const start = startDate ? moment(startDate) : moment().subtract(14, 'days')
+    const end = endDate ? moment(endDate) : moment()
+    const responses = await getResponses({ 
+      userId, channelId, start, end, client, say 
+    })
+    const { min, max, avg } = getStatistics({ responses })
+    await say({
+      text: `
+        User id ${userId}
+        Statistics for period 
+          from ${start.format('DD-MM-yyyy')} 
+          to ${end.format('DD-MM-yyyy')}:
+        Min response = ${min || 'no data'} 
+        Max response = ${max || 'no data'} 
+        Avg response = ${avg || 'no data'} 
+      `,
+      thread_ts
+    });
+    await say({ text: `Done.`, thread_ts })
+});
+
+
+app.command('/show-users-log', 
+  async ({ command, ack, say, client, ...r }) => {
+  await ack();
+  console.log({ command })
+  const message = await client.chat.postMessage({ 
+    text: 'Show users log: ',
+    channel: command.channel_id
+  })
+  const thread_ts = message.ts
+  
+  const [channel, seconds, startDate, endDate] = command.text.split(' ')
+  
+    let channelId = (channel.split('|')[0]).replace('<#', '')
+    console.log({ channel, channelId })
+    if (!channelId) {
+      await say({ 
+        text: 'Channel not found. Using current channel ' + 
+          command.channel,
+          thread_ts 
+      })
+      channelId = command.channel_id
+    } 
+    
+    await say({ text: `Analyzing...`, thread_ts })
+    const start = startDate ? moment(startDate) : moment().subtract(14, 'days')
+    const end = endDate ? moment(endDate) : moment()
+    
+    const usersList = await client.users.list()
+    const users = usersList.members
+    //const channelId = await channelToId({ channel, client })
+    
+    await Promise.all(users.map(async e => {
+      
+        const responses = await getResponses({ 
+          userId: e.id, channelId, start, end, client, say 
+        })
+       
+        if (!responses || !responses.length) {
+          return
+        }
+        
+        await Promise.all(responses.filter(r => r.rt > seconds)
+          .map(async responce => {
+            const link = await client.chat.getPermalink({ 
+              channel: channelId, message_ts: responce.ts
+            })
+            //console.log({ link })
+            await say({
+              text: `
+                User: ${e.name || 'name not found'}: 
+                Date: ${moment.unix(+responce.ts).format('DD-MM-yyyy hh:mm:ss') || 'time stamp not found'}
+                Response: ${humanizeDuration(moment.duration(responce.rt, 'seconds')) || 'responce time not found'}
+                Text: ${responce.text || 'text not found'}
+                Link: ${`<${link.permalink}|...>`}
+                =================
+                
+            `,
+              thread_ts
+            })
+          })
+        )
+      })
+    )
+    
+    await say({ text: `Done.`, thread_ts })
+});
+
 
 (async () => {
   await app.start(process.env.PORT || 3000);
